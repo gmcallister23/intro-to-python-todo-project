e2bef08a61c6ef13b319d9012ddc36c4e77189e2
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-02-04 17:30:32 -0500

    exception and error handling for add task

--- todo-app.py
+++ todo-app.py
@@ -9,10 +9,16 @@ def display_menu():
     print("4. Exit")
 
 def add_task():
-    new_task = input("What would you like ot add to your to-do list?")
-    task.append(new_task)
-    print(f"{new_task} has been added to your to-do list.")
-    print(task)
+
+    try:
+
+        new_task = input("What would you like ot add to your to-do list?")
+        task.append(new_task)
+        print(f"{new_task} has been added to your to-do list.")
+        print(task)
+
+    except Exception as e: 
+        print(f"An error has occurred: {e}")
 
 def view_task():
     if len(task) == 0:
