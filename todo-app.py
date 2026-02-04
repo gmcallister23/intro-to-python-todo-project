8259e8b913b39d0d3e8a9ca1b0cf60836167fabb
Graham McAllister <11613786+gmcallister23@users.noreply.github.com>
2026-02-04 17:12:44 -0500

    Setting up task list and display menu

---
+++ todo-app.py
@@ -0,0 +1,11 @@
+task = []
+
+def display_menu():
+    print("Welcome to your To-Do List!")
+    print("Please section an option:")
+    print("1. Add New Task")
+    print("2. View Tasks")
+    print("3. Delete Task")
+    print("4. Exit")
+
+    
